225f54d81a608a2d7beee073fd6f227e71dfee79
Anastasiia Shulichenko <nastusenka@gmail.com>
2017-12-25 16:40:33 +0100

    Initial commit

---
+++ src/main/java/com/klab/menubuilder/MenubuilderApplication.java
@@ -0,0 +1,12 @@
+package com.klab.menubuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenubuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenubuilderApplication.class, args);
+	}
+}
